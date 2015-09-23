790f3321ee4877b447663d00649ea706ac17a9d1
hwinn4 <hilary.winn@gmail.com>
2015-09-22 23:27:10 -0400

    all 3 methods complete

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,11 +21,11 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   emo_hash = load_library(file_path)
   emo_hash.each do |k, v|
-    possible_emoticons = v.keys
-    if possible_emoticons.include?(emoticon) == false
-      return "Sorry, that emoticon was not found"
-    end
     if k == "get_emoticon"
+      possible_emoticons = v.keys
+      if possible_emoticons.include?(emoticon) == false
+        return "Sorry, that emoticon was not found"
+      end
       v.each do |en, jp|
         if en == emoticon
           return jp
@@ -37,6 +37,45 @@ end
 
 
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  # store the hash of emoticons
+  emo_hash = load_library(file_path)
+  # iterate over the hash
+  emo_hash.each do |k, v|
+    # check if we are looking at the right nested hash
+    if k == "get_meaning"
+      # generate list of all keys in nested hash
+      possible_emoticons = v.keys
+      # if the argument is not an option, return sorry...
+      if possible_emoticons.include?(emoticon) == false
+        return "Sorry, that emoticon was not found"
+      end
+      # return the translation of the desired emoticon, if present in the hash
+      v.each do |jp, en|
+        if jp == emoticon
+          return en
+        end
+      end
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
