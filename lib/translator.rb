20f69f1f555689a9ed1b6cdefbb963f666a4a932
Adam King <adam.king0126@gmail.com>
2015-12-13 19:47:04 -0600

    passing first tests

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,16 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  translate_emoticon = {"get_meaning" => {},
+                        "get_emoticon" => {}}
+  emoticons.each do |k, v|
+    translate_emoticon['get_meaning'][v[1]] = k
+    translate_emoticon['get_emoticon'][v[0]] = v[1]
+  end
+  translate_emoticon
   # code goes here
 end
 
