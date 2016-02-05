031d3f43c6720cb51756ecf9d1b3f3a13d78bb4e
Anna Rasshivkina <anna.nigma@yahoo.com>
2016-02-05 15:06:20 -0500

    Refactor code

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,21 +1,36 @@
 require 'yaml'
+require 'pry'
 
 def load_library(file)
-  loaded_file = YAML.load_file(file)
-  library = { "get_meaning" => {}, "get_emoticon" => {} }
-  loaded_file.each do |meaning, emoticons|
-    library["get_meaning"][emoticons.last] = meaning
-    library["get_emoticon"][emoticons.first] = emoticons.last
+  emoticon_file = YAML.load_file(file)
+  emoticon_dic = { "get_meaning" => {}, "get_emoticon" => {} }
+  emoticon_file.each_with_object(emoticon_dic) do |(meaning, emoticons), dictionary|
+    dictionary["get_emoticon"][emoticons.first] = emoticons.last
+    dictionary["get_meaning"][emoticons.last] = meaning
   end
-  library
 end
 
-def get_japanese_emoticon(file, emoticon)
+def get_japanese_emoticon(file, english_emoticon)
   library = load_library(file)
-  library["get_emoticon"][emoticon] == nil ? "Sorry, that emoticon was not found" : library["get_emoticon"][emoticon]
+  japanese_emoticon = library["get_emoticon"][english_emoticon]
+  japanese_emoticon == nil ? "Sorry, that emoticon was not found" : japanese_emoticon
 end
 
-def get_english_meaning(file, emoticon)
+def get_english_meaning(file, japanese_emoticon)
   library = load_library(file)
-  library["get_meaning"][emoticon] == nil ? "Sorry, that emoticon was not found" : library["get_meaning"][emoticon]
+  english_emoticon = library["get_meaning"][japanese_emoticon]
+  english_emoticon == nil ? "Sorry, that emoticon was not found" : english_emoticon
 end
+
+
+# ORIGINAL METHOD
+#
+# def load_library(file)
+#   loaded_file = YAML.load_file(file)
+#   library = { "get_meaning" => {}, "get_emoticon" => {} }
+#   loaded_file.each do |meaning, emoticons|
+#     library["get_meaning"][emoticons.last] = meaning
+#     library["get_emoticon"][emoticons.first] = emoticons.last
+#   end
+#   library
+# end
