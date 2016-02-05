8433d962af523664d769863363d9117590688386
chintas1 <chintas1@tcnj.edu>
2016-02-05 09:54:12 -0500

    Refactors solution

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,36 +1,23 @@
 # require modules here
 require "yaml"
 def load_library(path)
-  # code goes here
-  translations = {}
-  translations['get_meaning']  = {}
-  translations['get_emoticon'] = {}
   file = YAML.load_file(path)
-  file.each do |meaning, emoticons|
+  file.each_with_object({'get_meaning' => {}, 'get_emoticon' => {}}) do |(meaning, emoticons),translations|
     translations["get_meaning"][emoticons[1]] = meaning.to_s
     translations["get_emoticon"][emoticons[0]] = emoticons[1]
   end
-  translations
 end
 
 def get_japanese_emoticon(path, emoticon)
-  # code goes here
   translations = load_library(path)
-  if (translations["get_emoticon"].keys.include?(emoticon))
-    translations["get_emoticon"][emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
+  error = "Sorry, that emoticon was not found"
+  (translations["get_emoticon"].keys.include?(emoticon)) ? translations["get_emoticon"][emoticon] : error
 end
 
 def get_english_meaning(path, emoticon)
-  # code goes here
   translations = load_library(path)
-  if (translations["get_meaning"].keys.include?(emoticon))
-    translations["get_meaning"][emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
+  error = "Sorry, that emoticon was not found"
+  (translations["get_meaning"].keys.include?(emoticon)) ? translations["get_meaning"][emoticon] : error
 end
 
 load_library("./lib/emoticons.yml")
